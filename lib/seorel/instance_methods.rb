0d1b21a8d38659cc65ec977084c08fb7b17000c4
Andrea Dal Ponte <dalpo85@gmail.com>
2012-08-29 11:00:22 +0200

    seorel default value

--- lib/seorel/instance_methods.rb
+++ lib/seorel/instance_methods.rb
@@ -1,16 +1,24 @@
 module Seorel
   module Base
     module InstanceMethods
-
       def seorel?
         self.seorel.present?
       end
 
       def set_empty_fields
-        self.seorel.title ||= self.send @seorel_base_field
-        self.seorel.description ||= self.send @seorel_base_field
+        if seorel_default_value?
+          self.seorel.title ||= seorel_default_value
+          self.seorel.description ||= seorel_default_value
+        end
       end
 
+      def seorel_default_value
+        self.send @seorel_base_field if seorel_default_value?
+      end
+
+      def seorel_default_value?
+        @seorel_base_field.present?
+      end
     end
   end
 end
