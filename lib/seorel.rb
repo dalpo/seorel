2daff396ae7ee7938d6e594216b1a43e38d8b0ca
Andrea Dal Ponte <dalpo85@gmail.com>
2013-02-04 12:05:54 +0100

    controller/instance_methods

--- lib/seorel.rb
+++ lib/seorel.rb
@@ -4,9 +4,10 @@ require "seorel/model/base"
 require "seorel/model/instance_methods"
 require "seorel/model/class_methods"
 
-require "seorel/controller/base"
 require "seorel/controller/instance_methods"
 require "seorel/controller/class_methods"
+
+require "seorel/seorelify"
 
 module Seorel
 end
--- lib/seorel/controller/instance_methods.rb
+++ lib/seorel/controller/instance_methods.rb
@@ -2,9 +2,34 @@ module Seorel
   module Controller
     module InstanceMethods
 
-      def add_meta(values = {})
+      def add_seorel_meta(obj = {})
+        if obj.class.name == 'Hash'
+          add_seorel_hash obj
+        elsif obj.respond_to? :seorel
+          add_seorel_model obj
+        else
+          raise "Seorel `add_seorel_meta` invalid argument"
+        end
+      end
+
+      def add_seorel_hash(values = {})
+        seosel_metatags = values
+      end
+
+      def add_seorel_model(model)
+        seosel_metatags = {
+          title: model.seo_title,
+          description: model.seo_description,
+          image: model.seo_image.default.url
+        }
+      end
+
+      def seosel_metatags
         @seosel_metatags ||= {}
-        @seosel_metatags.merge(values)
+      end
+
+      def seosel_metatags=(values = {})
+        @seosel_metatags = seosel_metatags.merge(values)
       end
 
     end
