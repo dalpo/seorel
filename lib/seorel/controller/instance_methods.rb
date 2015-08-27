4cf105ad2d409df27aa32fdd04df7142b2bc581b
Andrea Dal Ponte <info@andreadalponte.com>
2015-08-28 00:52:19 +0200

    Allow image assignation

--- lib/seorel/controller/instance_methods.rb
+++ lib/seorel/controller/instance_methods.rb
@@ -2,7 +2,6 @@
 module Seorel
   module Controller
     module InstanceMethods
-
       def add_seorel_meta(obj = {})
         if obj.class.name == 'Hash'
           add_seorel_hash obj
@@ -16,14 +15,14 @@ module Seorel
       def add_seorel_hash(values = {})
         seorel_params.title = values[:title] if values[:title].present?
         seorel_params.description = values[:description] if values[:description].present?
-        # seorel_params.image = values[:image] if values[:image].present?
+        seorel_params.image = values[:image] if values[:image].present?
         seorel_params
       end
 
       def add_seorel_model(model)
-        seorel_params.title = model.seo_title
-        seorel_params.description = model.seo_description
-        # seorel_params.image = model.seo_image.default.url if model.seo_image?
+        seorel_params.title = model.seo_title if model.seo_title?
+        seorel_params.description = model.seo_description if model.seo_description?
+        seorel_params.image = model.seo_image if model.seo_image?
         seorel_params
       end
 
@@ -36,7 +35,6 @@ module Seorel
         alias_method :add_metatags, :add_seorel_meta
         protected :add_seorel_meta, :add_seorel_hash, :add_seorel_model, :seorel_params
       end
-
     end
   end
 end
