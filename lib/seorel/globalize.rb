2cc4ce344fe662f9066562a8f614c8283f4112e7
Andrea Dal Ponte <info@andreadalponte.com>
2016-05-31 10:24:53 +0200

    Change: add base globalize configuration

---
+++ lib/seorel/globalize.rb
@@ -0,0 +1,15 @@
+require 'globalize'
+require 'seorel/seorel'
+require 'active_support/concern'
+
+module Seorel
+  module Globalize
+    extend ActiveSupport::Concern
+
+    included do
+      translates :title, :description, :image
+    end
+  end
+end
+
+Seorel::Seorel.include Seorel::Globalize
